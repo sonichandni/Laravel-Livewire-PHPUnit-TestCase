152c3ece12e2c90edafa6af3efaa797c34fda72d
Chandni Soni <sonichandni279@gmail.com>
2024-02-26 18:34:37 +0530

    add CRUD methods in component

--- app/Livewire/Post.php
+++ app/Livewire/Post.php
@@ -16,9 +16,112 @@ class Post extends Component
         'deletePostListner' => 'deletePost'
     ];
 
+    /**
+    * List of add/edit form rules 
+    */
+    protected $rules = [
+        'title' => 'required',
+        'description' => 'required'
+    ];
 
-    public function render()
-    {
+    /**
+    * Reseting all inputted fields
+    * @return void
+    */
+    public function resetFields() {
+        $this->title = '';
+        $this->description = '';
+    }
+
+    /**
+    * render the post data
+    * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+    */
+    public function render() {
+        $this->posts = Posts::select('id', 'title', 'description')->get();
         return view('livewire.post');
+    }
+
+    /**
+    * Open Add Post form
+    * @return void
+    */
+    public function addPost() {
+        $this->resetFields();
+        $this->addPost = true;
+        $this->updatePost = false;
+    }
+
+    /**
+    * store the user inputted post data in the posts table
+    * @return void
+    */
+    public function storePost() {
+        $this->validate();
+        Posts::create([
+            'title' => $this->title,
+            'description' => $this->description
+        ]);
+        session()->flash('success','Post Created Successfully!!');
+        $this->resetFields();
+        $this->addPost = false;
+    }
+
+    /**
+    * show existing post data in edit post form
+    * @param mixed $id
+    * @return void
+    */
+    public function editPost($id) {
+        $post = Posts::findOrFail($id);
+        if(empty($post)) {
+            session()->flash('error', 'Post not found!!');
+        } else {
+            $this->title = $post->title;
+            $this->description = $post->description;
+            $this->postId = $post->id;
+            $this->addPost = false;
+            $this->updatePost = true;
+        }
+    }
+
+    /**
+    * update the post data
+    * @return void
+    */
+    public function updatePost() {
+        $this->validate();
+        Posts::find($this->postId)->update([
+            'title' => $this->title,
+            'description' => $this->description
+        ]);
+        session()->flash('success', 'Post Updated Successfully!!');
+        $this->resetFields();
+        $this->updatePost = false;
+    }
+
+    /**
+    * Cancel Add/Edit form and redirect to post listing page
+    * @return void
+    */
+    public function cancel() {
+        $this->addPost = false;
+        $this->updatePost = false;
+        $this->resetFields();
+    }
+
+    /**
+    * delete specific post data from the posts table
+    * @param mixed $id
+    * @return void
+    */
+    public function deletePost($id) {
+        $post = Posts::findOrFail($id);
+        if(empty($post)) {
+            session()->flash('error', 'Post not found!!');
+        } else {
+            $post->delete();
+            session()->flash('success', 'Post Deleted Successfully!!');
+        }
     }
 }
